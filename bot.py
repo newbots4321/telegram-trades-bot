777f8cd0d87b642c5ddfd96e99f68316d3372f6e
newbots4321 <rishibhakatworkpro@gmail.com>
2025-07-14 14:40:33 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
-
 from telegram.ext import Updater, CommandHandler
+import imghdr
 
 def start(update, context):
-    update.message.reply_text("👋 Welcome to Trade Signal Bot!\nVisit for full analysis: https://cryptosignalmonetag.netlify.app")
+    update.message.reply_text("Hello! I’m alive with imghdr 😎")
 
-updater = Updater("7324799687:AAEw5jlN8mzaxI27lNBlSRgabfeszs4Xs8o", use_context=True)
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-updater.start_polling()
-updater.idle()
+def main():
+    TOKEN = "7324799687:AAEw5jlN8mzaxI27lNBlSRgabfeszs4Xs8o"
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
